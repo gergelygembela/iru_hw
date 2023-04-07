75e4da5080cf6e8d75126349ad29cf3e0c0ce453
Gergely Gembela <47938480+gergelygembela@users.noreply.github.com>
2023-04-07 17:56:20 +0200

    finds potentially relevant methods

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ import CppHeaderParser
 
 import pathlib
 
+
+class ResultItem:
+    name = 'a'
+    
+
 headers: list[CppHeaderParser.CppHeader] = []
 
+runResult: list[ResultItem] = []
 
 def absoluteFilePaths(directory: str):
     for dirpath,_,filenames in os.walk(directory):
@@ -14,10 +20,51 @@ def absoluteFilePaths(directory: str):
             yield os.path.abspath(os.path.join(dirpath, f))
 
 
+def findMethodsWithName(cls: CppHeaderParser.CppClass, name: str)-> list[CppHeaderParser.CppMethod]:
+    for method in cls.get('methods'):
+        print(method.get())
+
+
+def discoverParents(current: CppHeaderParser.CppClass, classes: list[CppHeaderParser.CppClass]) -> list[CppHeaderParser.CppClass]:
+    parents: list[CppHeaderParser.CppClass] = []
+    for cls in classes:
+        for parent in current.get('inherits'):
+            if cls.get('name') == parent.get('class'):
+                if cls not in parents: 
+                    parents.append(cls)
+                    parents += discoverParents(cls, classes)
+    return parents
+
+
+def compareMethods(lhs: CppHeaderParser.CppMethod, rhs: CppHeaderParser.CppMethod):
+
+    
+    return
+
+
+def findSimilarMethodsInClass(cls: CppHeaderParser.CppClass, other: CppHeaderParser.CppMethod):
+    for method in cls.get('methods').get('public'):
+        if(method.get('name') == other.get('name')):
+            compareMethods(method, other)
+    return
+
+
 def findSimilarMethods(classes: list[CppHeaderParser.CppClass], current: CppHeaderParser.CppClass):
     if(len(current.get('inherits')) == 0):
         return
     print("Processing methods of "+current.get('name'))
+    
+    #discover parent tree
+    parents = discoverParents(current, classes)
+ 
+    for method in current.get('methods').get('public'):
+        for cls in parents:
+            findSimilarMethodsInClass(cls, method)
+
+    for method in current.get('methods').get('protected'):
+        for cls in parents:
+            findSimilarMethodsInClass(cls, method)
+
 
 
 def processDir(dirPath: str):
@@ -28,6 +75,8 @@ def processDir(dirPath: str):
             if(pathlib.Path(entry).suffix == ".h"):
                 headers.append(CppHeaderParser.CppHeader(entry))
     
+
+
     classes: list[CppHeaderParser.CppClass] = []
 
     for header in headers:
---
+++ testlib/module.cpp
@@ -0,0 +1,8 @@
+#include "class5_3_4.h"
+
+int main(int argc, char** argv){
+    
+    class5 a;
+    a.do_something(42);
+    return 0;
+}
